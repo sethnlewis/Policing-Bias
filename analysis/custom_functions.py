93aa146f8b541aa99ddaafbdcbad7eb62be9b930
Seth Schober <seth.schober@gmail.com>
2021-05-02 16:15:40 -0600

    DecisionTreeClassifier implementation in progress

--- analysis/custom_functions.py
+++ analysis/custom_functions.py
@@ -106,3 +106,51 @@ def bar_plot(data, x_axis, y_axis, agg_type, verbose=False):
         
         print('--------------------------------------------------------------------\n\n')
     return
+
+
+
+# IN PROGRESS!!!!!
+def forward_selected(model, data, response):
+    """Linear model designed by forward selection.
+
+    SOURCE: https://planspace.org/20150423-forward_selection_with_statsmodels/
+    
+    Parameters:
+    -----------
+    data : pandas DataFrame with all possible predictors and response
+
+    response: string, name of response column in data
+
+    Returns:
+    --------
+    model: an "optimal" fitted statsmodels linear model
+           with an intercept
+           selected by forward selection
+           evaluated by adjusted R-squared
+    """
+    remaining = set(data.columns)
+    remaining.remove(response)
+    selected = []
+    current_score, best_new_score = 0.0, 0.0
+    while remaining and current_score == best_new_score:
+        scores_with_candidates = []
+        for candidate in remaining:
+            #formula = "{} ~ {} + 1".format(response,
+            #                               ' + '.join(selected + [candidate]))
+            #score = smf.ols(formula, data).fit().rsquared_adj
+            
+            #model = 
+            
+            #score = 
+            
+            scores_with_candidates.append((score, candidate))
+        scores_with_candidates.sort()
+        best_new_score, best_candidate = scores_with_candidates.pop()
+        if current_score < best_new_score:
+            remaining.remove(best_candidate)
+            selected.append(best_candidate)
+            current_score = best_new_score
+    formula = "{} ~ {} + 1".format(response,
+                                   ' + '.join(selected))
+    model = smf.ols(formula, data).fit()
+    return model
